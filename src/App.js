bc1b71d397710490761a35a9dd1456952241b60f
YASHJAGYANI <yashjagyani09@gmail.com>
2021-04-23 21:52:12 +0530

    commiting git commands

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <p>starting something great!</p>
     </div>
   );
 }
+
+// git add .
+// git commit -m "commit-msg"
+// git push origin main
+
+// go to left panel by clicking third icon (git icon)
+// click + button to add
+// write commit msg at top
+// click check at the top most panel to push
