4601aa37a07ef9cb037ec8a097e12e96b003cb28
YASHJAGYANI <yashjagyani09@gmail.com>
2021-04-24 20:37:28 +0530

    BOOK AND DISPLAY ADDED

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
 
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
